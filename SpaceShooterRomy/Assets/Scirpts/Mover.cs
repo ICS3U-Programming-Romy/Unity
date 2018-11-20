d786e9513affdbab7dcd48ca5e044787ba6ca935
Romy Chu III <romy.chu-iii@ocsbstudent.ca>
2018-11-20 09:32:38 -0500

    Added Even MORE stuff!
    -Fixed the player movement script
    -Added "Blolt" prefab

---
+++ SpaceShooterRomy/Assets/Scirpts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+}
