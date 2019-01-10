a52428173e24b0ee7b1ba8f3fa4d156d51e9ecc6
Romy Chu III <romy.chu-iii@ocsbstudent.ca>
2019-01-10 09:26:25 -0500

    2D Roguelike - Update 2.2
    Finished the second script, Game Manager

--- 2DRoguelikeRomyIII/2DRoguelikeRomyIII/Assets/Scipts/GameManager.cs
+++ 2DRoguelikeRomyIII/2DRoguelikeRomyIII/Assets/Scipts/GameManager.cs
@@ -1,16 +1,32 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static GameManager instance = null; //Static instance of GameManager which allows it to be accessed by any other script.
+    private BoardManager boardScript;   //Reference to the BoardManager, to set up the level.
+    private int level = 3; //The Level Number
+
+    void Awake()//Is called before Start function
+    {
+        boardScript = GetComponent<BoardManager>();//Get the attached BoardManager script
+        InitGame();
+
+        //Basically prevents there from being more that 1 gamemanager
+        if (instance == null)//Check if instance already exists
+            instance = this;//if not, set instance to this
+        else if (instance != this) //If instance already exists and it's not this
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);//Prevents this gameobject from being destroyed when reloading the scene
+    }
+
+    void InitGame()//Starts the game.
+    {
+        //Call the SetupScene function of the BoardManager script.
+        boardScript.SetupScene(level);
+
+    }
 }
